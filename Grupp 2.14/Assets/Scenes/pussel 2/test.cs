7c22e5421e86041a800f07f684cf588e503f3670
Submarinene <submarinenene@gmail.com>
2026-01-12 10:17:43 +0100

    Merge Pernille

---
+++ Grupp 2.14/Assets/Scenes/pussel 2/test.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class test : MonoBehaviour
+{
+    //lista med typ rigidbody(istället för img) på alla collumner 
+    //lista på outlines
+    [SerializeField]
+    GameObject[] cA;
+
+    void Start()
+    {
+      /*  for (int i = 0; i < cA.Length; i++)
+        {
+            cA(i).SetActive(true);
+        }*/
+
+    }
+
+    public void OnMouseDown()
+    {
+
+
+       
+
+      /* for(int i = 0; i < collumnA.Length; i++)
+        {
+            collumnA(i).SetActive(false);
+        }
+        */
+    }
+
+
+
+}
---
+++ Grupp 2.14/Assets/Scenes/start/Meny.cs
@@ -0,0 +1,34 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Meny : MonoBehaviour
+{
+  public void start()
+    {
+        SceneManager.LoadSceneAsync(0);
+    }
+    public void pussel1()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+    public void pussel2()
+    {
+        SceneManager.LoadSceneAsync(2);
+    }
+    public void pussel3()
+    {
+        SceneManager.LoadSceneAsync(3);
+    }
+    public void TeaterScen1()
+    {
+        SceneManager.LoadSceneAsync(4);
+    }
+    public void TeaterScen2()
+    {
+        SceneManager.LoadSceneAsync(5);
+    }
+
+}
