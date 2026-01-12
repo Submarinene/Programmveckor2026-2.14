935eae27901284ae1c40e87dfa7359cd71f9e9de
Submarinene <submarinenene@gmail.com>
2026-01-12 09:29:16 +0100

    idk

--- Grupp 2.14/Assets/Scenes/pussel 2/test.cs
+++ Grupp 2.14/Assets/Scenes/pussel 2/test.cs
@@ -1,10 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class test : MonoBehaviour
 {
-     //lista med typ rigidbody(istället för img) på alla collumner 
-     //lista på outlines
-    
+    //lista med typ rigidbody(istället för img) på alla collumner 
+    //lista på outlines
+    [SerializeField]
+    GameObject[] collumnA;
+
+    void Start()
+    {
+        for (int i = 0; i < collumnA.Length; i++)
+        {
+            collumnA(i).SetActive(true);
+        }
+
+    }
+
+    public void OnMouseDown()
+    {
+      
+
+
+
+      /* for(int i = 0; i < collumnA.Length; i++)
+        {
+            collumnA(i).SetActive(false);
+        }
+        */
+    }
 
 
 
