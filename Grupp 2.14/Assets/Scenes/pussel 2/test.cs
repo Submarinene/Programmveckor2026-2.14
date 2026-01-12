b34d439c0961f6d12779ea236dbd740f542aa9b6
Submarinene <submarinenene@gmail.com>
2026-01-12 12:14:50 +0100

    fixade columnerna lite

--- Grupp 2.14/Assets/Scenes/pussel 2/test.cs
+++ Grupp 2.14/Assets/Scenes/pussel 2/test.cs
@@ -9,13 +9,44 @@ public class test : MonoBehaviour
     //lista med typ rigidbody(istället för img) på alla collumner 
     //lista på outlines
     [SerializeField]
-    GameObject[] cA;
+    GameObject[] cA; //column A
+
+    [SerializeField]
+    GameObject[] cB; //column B
+
+    [SerializeField]
+    GameObject[] cC; //column C
+
+    [SerializeField]
+    GameObject[] cD; //column D
+
+    [SerializeField]
+    GameObject[] cE; //column E
+
+    [SerializeField]
+    GameObject[] cF; //column F
+
+    [SerializeField]
+    GameObject[] cG; //column G
+
+    [SerializeField]
+    GameObject[] cH; //column H
+
+    [SerializeField]
+    GameObject[] cI; //column I
+
+    [SerializeField]
+    GameObject[] cJ; //column J
+
+    [SerializeField]
+    GameObject[] antalOrd;
+  
 
     void Start()
     {
-      /*  for (int i = 0; i < cA.Length; i++)
+      /*  for (int i = 0; i < antalOrd.Length; i++)
         {
-            cA(i).SetActive(true);
+            antalOrd(i).SetActive(false);
         }*/
 
     }
@@ -24,13 +55,9 @@ public class test : MonoBehaviour
     {
 
 
-       
+        Debug.Log("det funkar att klicka");
 
-      /* for(int i = 0; i < collumnA.Length; i++)
-        {
-            collumnA(i).SetActive(false);
-        }
-        */
+  
     }
 
 
