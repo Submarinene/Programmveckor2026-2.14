fdad61bd56353719ccb87cc0862f1c5b25d37bcf
Jhnrfael <john.mendozatovera@elev.ga.lbs.se>
2026-01-15 14:03:42 +0100

    cutscene galore

--- Grupp 2.14/Assets/Scenes/start/Test.cs
+++ Grupp 2.14/Assets/Scenes/start/Test.cs
@@ -7,14 +7,32 @@ public class Test : MonoBehaviour
     [SerializeField] private Animator sceneTransitionAnimator;
     [SerializeField] private string endTransitionTrigger = "FadeIn";
     [SerializeField] private float transitionDuration = 1.0f;
+    [SerializeField] private GameObject startButton;
+    [SerializeField] private GameObject quitButton;
 
     public void LoadCutscene()
     {
         StartCoroutine(TransitionToCutscene());
     }
 
+    void Start()
+    {
+        // Hide buttons on startup
+        if (startButton != null)
+            startButton.SetActive(false);
+        if (quitButton != null)
+            quitButton.SetActive(false);
+            StartCoroutine(StartUpTransition());
+    }
+
     private IEnumerator TransitionToCutscene()
     {
+        // Hide buttons before starting fade
+        if (startButton != null)
+            startButton.SetActive(false);
+        if (quitButton != null)
+            quitButton.SetActive(false);
+
         // Trigger the end transition (fade to black)
         if (sceneTransitionAnimator != null)
         {
@@ -23,5 +41,17 @@ public class Test : MonoBehaviour
         }
 
         SceneManager.LoadScene("Startup Cutscene");
+    }
+
+    private IEnumerator StartUpTransition()
+    {
+        if(sceneTransitionAnimator != null)
+        {
+            yield return new WaitForSeconds(transitionDuration);
+            if (startButton != null)
+                startButton.SetActive(true);
+            if (quitButton != null)
+                quitButton.SetActive(true);
+        }
     }
 }
