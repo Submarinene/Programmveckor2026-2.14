5c559bb96b503accf72a9af22f4b4ae753585ad4
EstellaBriones <estella.vallecerabriones@elev.ga.lbs.se>
2026-01-16 01:23:21 +0100

    Finished Cutscene 3 and added a scene where you cross the bridge

---
+++ Grupp 2.14/Assets/Scenes/CutSCENES/Cutscene 1/Scripts/LoadPuzzle3.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class LoadPuzzle3 : MonoBehaviour
+{
+    [SerializeField] private Animator sceneTransitionAnimator;
+    [SerializeField] private string endTransitionTrigger = "FadeIn";
+    [SerializeField] private float transitionDuration = 1.0f;
+    [SerializeField] int dragonSceneIndex;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Piece"))
+        {
+            StartCoroutine(TransitionToCutscene());
+        }
+    }
+
+    private IEnumerator TransitionToCutscene()
+    {
+        // Trigger the end transition (fade to black)
+        if (sceneTransitionAnimator != null)
+        {
+            sceneTransitionAnimator.SetTrigger(endTransitionTrigger);
+            yield return new WaitForSeconds(transitionDuration);
+        }
+
+        SceneManager.LoadScene(dragonSceneIndex);
+    }
+}
